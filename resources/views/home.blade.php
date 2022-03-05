3c6afa94764444019b28c02897e0083fa7b0d09b
abdallah-shraf <abdalla.ashraf@yahoo.com>
2022-03-05 18:58:08 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo List</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body class="bg-info">
+    <div class="container w-60 mt-5">
+        <div class="card shadow-sm">
+            <h3>To-do List</h3>
+            <form action="{{ route('store') }}" method="POST" outocompete="off">
+                @csrf
+                <div class="input-group">
+                    <input type="text" name="content" class="form-control" placeholder="Add your new todo" outocompete="off">
+                    <button class="btn btn-dark btn-sm px-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                      </svg></button>
+                </div>
+            </form>
+            @if (count($todolists))
+            <ul class="list-group list-group-flush mt-3">
+                @foreach ($todolists as $todolist )
+                    <li class="list-group-item">
+                        <form action="{{ route('destroy', $todolist->id) }}" method="POST" >
+                            {{ $todolist->content}}
+                            @csrf
+                            @method('delete')
+                            <button class="btn btn-link btn-sm float-end">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                </svg>
+                            </button>
+                        </form>
+                    </li>
+                @endforeach
+            </ul>
+            @else
+            <p class="text-center mt-3">No Tasks!</p>
+            @endif
+        </div>
+        @if (count($todolists))
+            <div class="card-footer">
+                You have {{ count($todolists)}}
+            </div>
+
+        @endif
+    </div>
+</body>
+</html>
